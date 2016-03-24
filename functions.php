b3d0c4fe4f02951143a32c8040bd2a5b68522f0e
Alice Guan <aliguan726@gmail.com>
2016-03-24 16:57:12 -0400

    add image size to functions.php

--- functions.php
+++ functions.php
@@ -49,6 +49,7 @@ add_theme_support('menus');
 /*--- Enable Post Thumbnails ---*/
 
 add_theme_support( 'post-thumbnails' );
+add_image_size( 'frontpg-thumb', 700, 477, true );
 
 
 ?>
--- index.php
+++ index.php
@@ -8,10 +8,10 @@
                     if ( have_posts() ) {
                     while ( have_posts() ) {
                         the_post(); ?>
-                    <h3><a href="<?php the_permalink();?>"><?php the_title();?></a></h3>
+                        <h3><a href="<?php the_permalink();?>"><?php the_title();?></a></h3>
                     <?php
                         if ( has_post_thumbnail() ) {
-                            the_post_thumbnail('thumbnail');
+                            the_post_thumbnail('frontpg-thumb');
                         }
                         the_excerpt();
                     } // end while
