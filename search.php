aadcae0d534816786624e9b86dafb39dc28e2d94
Alice Guan <aliguan726@gmail.com>
2016-03-27 15:56:25 -0400

    add padding to search results

--- search.php
+++ search.php
@@ -8,16 +8,18 @@ get_header(); ?>
             <h1><?php printf(__('Search Results for: %s'), '<span>' . get_search_query() . '</span>' ); ?></h1>
             <?php if (have_posts()) :
                 while (have_posts()) : the_post(); ?>
-                        <div class="four columns">
+                        <div class="searchpic">
                             <?php the_post_thumbnail(); ?>
                         </div>
+                        <div class="searchcontent">
                             <h2><?php the_title(); ?></h2>
-                            <?php the_excerpt();
+                            <?php the_excerpt(); ?>
+                        </div>
+
+                            <?
                             endwhile;
                             else : ?>
-
                             <h1>Nothing Found</h1>
                             <p>Sorry, but nothing matched your search criteria. Please try again with different search terms.</p>
                         <?php endif; ?>
-
-                </div>
+        </div>
